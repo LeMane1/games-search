d41b79580252caca634408ca863f95c6bd7c2afb
Aleksandr Zelenin <zeleninaleksandr@MacBook-Pro-Zelenin.local>
2025-03-26 20:11:24 +0300

    added shielding for user inputs in Login and Register at server side

--- src/app/login/login-container/lib/actions.ts
+++ src/app/login/login-container/lib/actions.ts
@@ -3,14 +3,15 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
 import {getUser} from "@/api/getUser";
+import sanitizeHtml from 'sanitize-html';
 
 export async function login(formData: FormData) {
   const supabase = await createClient()
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
+    email: sanitizeHtml(formData.get('email') as string),
+    password: sanitizeHtml(formData.get('password') as string),
   }
   
   const { error } = await supabase.auth.signInWithPassword(data)
@@ -26,9 +27,9 @@ export async function signup(formData: FormData) {
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-    userName: formData.get('name') as string,
+    email: sanitizeHtml(formData.get('email') as string),
+    password: sanitizeHtml(formData.get('password') as string),
+    userName: sanitizeHtml(formData.get('name') as string),
   }
   
   const { error } = await supabase.auth.signUp(data)
