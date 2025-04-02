e87c1dc2965c376ae4e31fa300e62bb928363576
Aleksandr Zelenin <zeleninaleksandr@MacBook-Pro-Zelenin.local>
2025-04-02 22:11:16 +0300

    added Home button for Error Page

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,4 +1,4 @@
-import {Stack, Typography} from "@mui/material";
+import {Button, Stack, Typography} from "@mui/material";
 import {BackButton} from "@/app/error/back-button";
 
 export default function ErrorPage(){
@@ -7,7 +7,16 @@ export default function ErrorPage(){
       <Typography variant="h5" component="div" textAlign="center">
         Something went wrong. Please try again.
       </Typography>
-      <BackButton />
+      
+      <Stack
+        direction='row'
+        justifyContent='center'
+        alignItems='center'
+        spacing={2}
+      >
+        <Button variant={'contained'} component='a' href={'/'}>Home</Button>
+        <BackButton />
+      </Stack>
     </Stack>
   )
 }
