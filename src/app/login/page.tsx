448b473b80e7862066f0e26cdb4bba1b6c79e5ea
Aleksandr Zelenin <zeleninaleksandr@MacBook-Pro-Zelenin.local>
2025-04-02 16:47:20 +0300

    added check for auth for Profile and Login Pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,13 @@
 import LoginContainer from "@/app/login/login-container";
+import {checkAuth} from "@/api/getUser";
+import {redirect} from "next/navigation";
 
 export default async function LoginPage() {
+  const isAuthenticated = await checkAuth()
+  if (isAuthenticated){
+    redirect('/')
+  }
+  
   return (
     <main style={{width: '100%'}}>
       <LoginContainer/>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,8 +2,15 @@ import BgOverlay from "@/components/bg-overlay";
 import ProfileInfo from "@/app/profile/profile-info";
 import GamesCollection from "@/app/profile/games-collection";
 import {Stack} from "@mui/material";
+import {checkAuth} from "@/api/getUser";
+import {redirect} from "next/navigation";
 
-export default function ProfilePage() {
+export default async function ProfilePage() {
+  const isAuthenticated = await checkAuth()
+  if (!isAuthenticated){
+    redirect('/')
+  }
+  
   return (
     <main style={{width: '100%'}}>
       <BgOverlay imageName={'/images/games_bg.jpg'}/>
