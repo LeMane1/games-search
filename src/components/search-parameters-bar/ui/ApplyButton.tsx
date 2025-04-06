18313ff78d113bb36fdbb2a967f7903c091424b5
Aleksandr Zelenin <zeleninaleksandr@MacBook-Pro-Zelenin.local>
2025-04-06 22:25:53 +0300

    added action for Apply Button

--- src/components/search-parameters-bar/ui/ApplyButton.tsx
+++ src/components/search-parameters-bar/ui/ApplyButton.tsx
@@ -1,33 +1,38 @@
 'use client'
 
 import {Button} from "@mui/material";
-import {useSearchParams} from "next/navigation";
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import {useAppSelector} from "@/lib/hooks";
+import {RootState} from "@/lib/store";
 
 export default function ApplyButton(){
-  //const searchParams = useSearchParams();
-  // const pathname = usePathname();
-  // const { replace } = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const {sortOrdering, selectedPlatforms} = useAppSelector((state: RootState) => state.searchParametersReducer);
   
   const handleOnClick = () => {
-  
+    const params = new URLSearchParams(searchParams);
+    
+    if (sortOrdering){
+      params.set('ordering', sortOrdering);
+    } else {
+      params.delete('ordering');
+    }
+    
+    if (selectedPlatforms.length > 0){
+      const platformsString = selectedPlatforms.join(',');
+      params.set('parent_platforms', platformsString);
+    }else{
+      params.delete('platforms');
+    }
+    
+    if (params.get('page')){
+      params.set('page', '1');
+    }
+    
+    replace(`${pathname}?${params.toString()}`);
   }
-  
-  // const handleChange = useDebouncedCallback((event: SelectChangeEvent) => {
-  //   const sortValue:string = event.target.value
-  //   const params = new URLSearchParams(searchParams);
-  //
-  //   if (sortValue) {
-  //     params.set('ordering', sortValue);
-  //   } else {
-  //     params.delete('ordering');
-  //   }
-  //
-  //   if (params.get('page')){
-  //     params.set('page', '1');
-  //   }
-  //
-  //   replace(`${pathname}?${params.toString()}`);
-  // }, 1000)
   
   return (
     <Button
