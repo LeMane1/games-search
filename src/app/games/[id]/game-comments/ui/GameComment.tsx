03dd0eb69d635dfe1734d0a6fc1b25fff2330d32
Aleksandr Zelenin <zeleninaleksandr@MacBook-Pro-Zelenin.local>
2025-04-09 23:32:29 +0300

    updated text in GameComment

--- src/app/games/[id]/game-comments/ui/GameComment.tsx
+++ src/app/games/[id]/game-comments/ui/GameComment.tsx
@@ -28,7 +28,7 @@ export default function GameComment({header, text, userName, createdAt, imageUrl
       
       {
         text &&
-        <Typography component='p' variant='body2' color='textSecondary' mb={2} whiteSpace={'break-spaces'}>
+        <Typography component='p' variant='body1' color='textSecondary' mb={2} whiteSpace={'break-spaces'}>
           {removeTagsFromString(text)}
         </Typography>
       }
